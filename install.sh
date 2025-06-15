13ebaae305e531a6817e779815af16b5abaf0160
MDev123 <86262715+MDev123@users.noreply.github.com>
2025-06-14 22:22:42 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Current dir: $(pwd)"
+ls -la
+ls -la server
+
 apt update -y && apt upgrade -y && apt autoremove -y && apt autoclean -y
 apt-get install default-jdk -y
 
-cd server
-curl -o server/server.jar https://papermc.io/api/v2/projects/paper/versions/1.8.8/builds/455/downloads/paper-1.8.8-455.jar
+mkdir -p server
+curl -L -o server/server.jar https://papermc.io/api/v2/projects/paper/versions/1.8.8/builds/455/downloads/paper-1.8.8-455.jar
